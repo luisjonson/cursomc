aa3438432dcafd530586247c4726441a0cfa850f
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2024-01-28 21:25:51 -0300

    Rest testado.

---
+++ src/main/java/com/treinamento/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package com.treinamento.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String listar() {
+		return "Rest esta fucionando";
+	}
+	
+	
+
+}
